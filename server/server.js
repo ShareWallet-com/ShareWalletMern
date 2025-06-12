aad333f22c735d01c001e7497140e48311902158
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-06-12 21:39:31 +0530

    Enhance CORS configuration in server.js to allow requests with no origin and improve error handling for disallowed origins

--- server/server.js
+++ server/server.js
@@ -16,7 +16,17 @@ const allowedOrigins = ['http://localhost:5173','https://sharewalletmern-fronten
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin:allowedOrigins, credentials:true}));
+app.use(cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true
+  }));
 
 //api Endpoints
 
