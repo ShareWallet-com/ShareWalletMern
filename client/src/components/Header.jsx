41ed7b2c32336c69cb517871fb3b2731abdff2bf
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-06-13 02:11:21 +0530

    Update Header component styling and user greeting logic for improved UI consistency and user experience.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,10 +6,10 @@ function Header() {
   const {userData} = useContext(AppContent);
 
   return (
-    <div className="flex items-center justify-center bg-gray-100 h-[90vh]">
+    <div className="flex items-center justify-center bg-[#333A5C] h-[90vh]">
   <div className="p-6 text-center bg-white shadow-xl rounded-2xl">
     <h1 className="mb-4 text-2xl font-bold">
-      Hey {userData ? userData.name : "Developer"}, Welcome to <span className="text-indigo-600">ShareWallet</span>
+      Hey {userData ? userData.name : " "}, Welcome to <span className="text-indigo-600">ShareWallet</span>
     </h1>
     <h2 className="mb-6 text-lg text-gray-600">Soon We Launch</h2>
     {/* <button className="px-6 py-2 text-white transition bg-indigo-600 rounded-full hover:bg-indigo-700">
