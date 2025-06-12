57a3a0054f4a544b129f6237edf9b6c70bdc35b8
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-06-12 22:41:03 +0530

    Fix API endpoint in getUserData function to correctly fetch user data

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -28,7 +28,7 @@ export const AppContextProvider = (props)=>{
 
     const getUserData = async () => {
   try {
-    const res = await axios.get(backendUrl + 'api/user/data', {
+    const res = await axios.get(`${backendUrl}/api/auth/is-auth`, {
       withCredentials: true
     });
 
