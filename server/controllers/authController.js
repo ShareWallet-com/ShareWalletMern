4cb3e382ad44cf9d2a1744a4d9c245d124ebcbc9
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-06-12 22:00:06 +0530

    Update environment configuration for production deployment and adjust cookie settings in authController for enhanced security in production environment

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -39,8 +39,8 @@ export const register = async(req,res)=>{
 
         res.cookie('token',token,{
             httpOnly:true,
-            secure: true,
-            sameSite: "None",
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge:7 * 24 * 60 * 60 * 1000 // 7 days
         });
 
