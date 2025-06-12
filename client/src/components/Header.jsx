4cf29ea6ec64dad7f56c5b792c7c0b2fd2f1f856
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-06-13 02:12:49 +0530

    Update Header component background color for improved visual appeal and consistency in user interface.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   const {userData} = useContext(AppContent);
 
   return (
-    <div className="flex items-center justify-center bg-[#333A5C] h-[90vh]">
+    <div className="flex items-center justify-center bg-gray-100 h-[90vh]">
   <div className="p-6 text-center bg-white shadow-xl rounded-2xl">
     <h1 className="mb-4 text-2xl font-bold">
       Hey {userData ? userData.name : " "}, Welcome to <span className="text-indigo-600">ShareWallet</span>
