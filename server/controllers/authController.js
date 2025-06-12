0ec6bc966dc99dc069d1d46db2a13a1adbace2c8
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-06-12 23:27:00 +0530

    Update API endpoint formatting in Navbar component and enhance cookie settings in authController for production environment

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -37,12 +37,13 @@ export const register = async(req,res)=>{
 
         const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
 
-        res.cookie("token", token, {
+        res.cookie('token', token, {
             httpOnly: true,
-            secure: true,            // Always true for production
-            sameSite: "None",        // Required for cross-site cookies
-            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+            secure: process.env.NODE_ENV === 'production', // use `false` locally
+            sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // allow cross-origin in prod
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
           });
+          
 console.log("Token set in cookie:", token);          
 
         const mailOptions = {
