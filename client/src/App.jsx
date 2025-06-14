6dab0c004ee85088f771a75d6a2b827e7c4ed834
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-06-14 13:35:26 +0530

    Fix route path casing for Dashboard in App and Login components for consistency

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import EmailVerify from './pages/EmailVerify'
 import ResetPassword from './pages/ResetPassword'
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import Dashboard from './components/Dashboard'
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Route path='/login' element={<Login/>}/>
         <Route path='/email-verify' element={<EmailVerify/>}/>
         <Route path='/reset-passsword' element={<ResetPassword/>}/>
+        <Route path='/dashboard' element={Dashboard}/>
       </Routes>
     </div>
   )
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -35,7 +35,7 @@ function Login() {
                 if(data.success){
                     setIsLoggedin(true);
                     await getUserData();
-                    navigate('/Dashboard');
+                    navigate('/dashboard');
                 } else {
                     toast.error(data.message)
                 }
@@ -48,7 +48,7 @@ function Login() {
                 if (data.success) {
                         setIsLoggedin(true);
                         await getUserData();
-                        navigate('/Dashboard');
+                        navigate('/dashboard');
                         }
  else {
                     toast.error(data.message)
