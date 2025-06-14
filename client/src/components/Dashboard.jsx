f91398acb8546fc0141907c97501d9e22a5e5bb6
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-06-14 14:06:51 +0530

    Refactor Dashboard component layout for improved structure and readability

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -4,22 +4,24 @@ import Navbar from './Navbar';
 
 function Dashboard() {
   const { userData } = useContext(AppContent);
-  return (
-      
-    <div className="flex items-center justify-center bg-gray-100 h-[90vh]">
+return (
+  <>
+    <div>
       <Navbar/>
-  <div className="p-6 text-center bg-white shadow-xl rounded-2xl">
-    <h1 className="mb-4 text-2xl font-bold">
-      Hey {userData ? userData.name : " "}, Welcome to <span className="text-indigo-600">ShareWallet</span>
-    </h1>
-    <h2 className="mb-6 text-lg text-gray-600">Soon We Launch</h2>
-    {/* <button className="px-6 py-2 text-white transition bg-indigo-600 rounded-full hover:bg-indigo-700">
-      Get Started
-    </button> */}
-  </div>
-</div>
-
-  )
+    </div>
+    <div className="flex flex-col items-center justify-center bg-gray-100 h-[90vh]">
+      <div className="p-6 text-center bg-white shadow-xl rounded-2xl">
+        <h1 className="mb-4 text-2xl font-bold">
+          Hey {userData ? userData.name : " "}, Welcome to <span className="text-indigo-600">ShareWallet</span>
+        </h1>
+        <h2 className="mb-6 text-lg text-gray-600">Soon We Launch</h2>
+        {/* <button className="px-6 py-2 text-white transition bg-indigo-600 rounded-full hover:bg-indigo-700">
+          Get Started
+        </button> */}
+      </div>
+    </div>
+  </>
+)
 }
 
 export default Dashboard
