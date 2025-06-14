e90a0f1f81e95cbfc1ab3df8f95845b21ef9fba8
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-06-14 14:44:40 +0530

    Fix layout in Header2 component for consistent button arrangement

--- client/src/components/Header2.jsx
+++ client/src/components/Header2.jsx
@@ -15,7 +15,7 @@ function Header2() {
             </h1>
             <p className='text-sm'>Save together, borrow interest-free, and grow your shared wealth—all in one wallet.</p>
         </div>
-        <div className="flex flex-col items-center justify-center w-full gap-4 px-4 sm:flex-col">
+        <div className="flex flex-row items-center justify-center w-full gap-4 px-4 sm:flex-row">
           <button className="w-full px-6 py-2 text-sm text-white bg-black border-2 sm:w-auto rounded-xl sm:text-base">
             Join Now
           </button>
