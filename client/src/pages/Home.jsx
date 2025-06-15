aa78bf6496045ba8ea99bbf5688b5a3a91443ae5
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-06-16 01:31:41 +0530

    Remove unused Header component import from Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Header from '../components/Header'
 import Header2 from '../components/Header2'
 import Dashboard from '../components/Dashboard'
 import Page2 from '../components/Page2'
