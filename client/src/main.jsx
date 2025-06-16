a470f8ee0c0b9e8024187d59bf6572b5f834f0d7
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-06-16 10:24:04 +0530

    Refactor routing setup to replace HashRouter with BrowserRouter for improved URL handling

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom';
 import { AppContextProvider } from './context/AppContext.jsx'
+import { BrowserRouter } from 'react-router-dom';
+
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <AppContextProvider>
-      <App/>
-    </AppContextProvider>
-  </HashRouter>,
-)
+  <StrictMode>
+    <BrowserRouter>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
